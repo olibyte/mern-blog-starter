9b7713ed376790937b1b519738c62778d50fb1f5
Oliver <ocben1@gmail.com>
2019-07-07 20:25:14 +1000

    create app component set up react router

---
+++ src/pages/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+    <h1>New blog coming soon!</h1>
+    <p>
+        Welcome to my blog. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget mi proin sed libero. Vestibulum mattis ullamcorper velit sed. Accumsan in nisl nisi scelerisque. Donec ac odio tempor orci. Sit amet mauris commodo quis imperdiet massa tincidunt. Nisl tincidunt eget nullam non nisi est. Volutpat diam ut venenatis tellus in metus vulputate. Leo vel fringilla est ullamcorper eget. Nunc pulvinar sapien et ligula ullamcorper malesuada. Viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat.
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget mi proin sed libero. Vestibulum mattis ullamcorper velit sed. Accumsan in nisl nisi scelerisque. Donec ac odio tempor orci. Sit amet mauris commodo quis imperdiet massa tincidunt. Nisl tincidunt eget nullam non nisi est. Volutpat diam ut venenatis tellus in metus vulputate. Leo vel fringilla est ullamcorper eget. Nunc pulvinar sapien et ligula ullamcorper malesuada. Viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat.
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget mi proin sed libero. Vestibulum mattis ullamcorper velit sed. Accumsan in nisl nisi scelerisque. Donec ac odio tempor orci. Sit amet mauris commodo quis imperdiet massa tincidunt. Nisl tincidunt eget nullam non nisi est. Volutpat diam ut venenatis tellus in metus vulputate. Leo vel fringilla est ullamcorper eget. Nunc pulvinar sapien et ligula ullamcorper malesuada. Viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat.
+    </p>
+    </>
+);
+
+export default HomePage;
