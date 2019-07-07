4e9d516ad1caabc3942890e7b7d58236d0ff505c
Oliver <ocben1@gmail.com>
2019-07-07 20:53:17 +1000

    added blog pages

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import {
   Route,
 } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesList from './pages/ArticlesList';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path="/" component={HomePage} exact />
+        <div id="page-body">
+          <Route path="/" component={HomePage} exact />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/articles-list" component={ArticlesList} />
+          <Route path="/article" component={ArticlePage} />
+        </div>
       </div>
     </Router>
   );
