24919dd62786f90f2a086316103c80a50d8445ca
Oliver <ocben1@gmail.com>
2019-07-07 21:22:16 +1000

    added article-content and url parameters with react-router

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import articleContent from './article-content';
 
-const ArticlePage = () => (
+const ArticlePage = ({ match }) => {
+    const name = match.params.name;
+    const article = articleContent.find(article => article.name === name);
+    
+    if(!article) return <h1>Oops! Article not found.</h1>
+
+    return (
     <>
-    <h1>This is an article</h1> 
+    <h1>{article.title}</h1>
+    {article.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+    ))}
     </>
 );
-
+}
 export default ArticlePage;
