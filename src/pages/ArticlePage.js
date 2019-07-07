dc49988f94015274298b3630883e6ed71bb29ed1
Oliver <ocben1@gmail.com>
2019-07-07 22:52:35 +1000

    added 404 page

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ArticlesList from '../components/ArticlesList';
 import articleContent from './article-content';
+import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
     
-    if(!article) return <h1>Oops! Article not found.</h1>
+    if(!article) return <NotFoundPage />
 
     const otherArticles = articleContent.filter(article => article.name !== name);
 
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFoundPage = () => (
+    <h1>Article not found!</h1>
+);
+
+export default NotFoundPage;
