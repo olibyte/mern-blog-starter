ee04999ad3d638af4c0bb70e38a54416a096776f
Oliver <ocben1@gmail.com>
2019-07-07 22:41:01 +1000

    made articles list modular

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ArticlesList from '../components/ArticlesList';
 import articleContent from './article-content';
 
 const ArticlePage = ({ match }) => {
@@ -7,12 +8,16 @@ const ArticlePage = ({ match }) => {
     
     if(!article) return <h1>Oops! Article not found.</h1>
 
+    const otherArticles = articleContent.filter(article => article.name !== name);
+
     return (
     <>
     <h1>{article.title}</h1>
     {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
     ))}
+    <h3>Other Articles: </h3>
+    <ArticlesList articles={otherArticles} />
     </>
 );
 }
