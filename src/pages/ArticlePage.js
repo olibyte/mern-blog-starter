9a7adb7238facea66f5b75ca8e3d92c79f3827c8
Oliver <ocben1@gmail.com>
2019-07-09 10:34:52 +1000

    added CommentsList component

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ArticlesList from '../components/ArticlesList';
 import articleContent from './article-content';
+import CommentsList from '../components/CommentsList';
 import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = ({ match }) => {
@@ -30,6 +31,7 @@ const ArticlePage = ({ match }) => {
     {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
     ))}
+    <CommentsList comments={articleInfo.comments} />
     <h3>Other Articles: </h3>
     <ArticlesList articles={otherArticles} />
     </>
